801ff8bac23feb9bf3fcd19dc3f1b4b03b06a7cf
mario <mario@vevisystems.com>
2024-07-29 10:53:21 +0200

    añadida funcionalidad para editar y borrar mascotas

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -21,6 +21,25 @@ class PetsController < ApplicationController
     end
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    if @pet.update(pet_params)
+      redirect_to pets_path, notice: "se ha actualizado correctamente"
+    else
+      render :edit, notice: 'Pet was not updated.'
+    end
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+    redirect_to pets_path, notice: 'Pet was successfully destroyed.'
+  end
+
   private
   def pet_params
     params.require(:pet).permit(:name, :animal_type, :breed, :age, :chip_number, :client_id)
